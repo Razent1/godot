80f10788d040e67b6bbbb0bbeac78bdb33670dbd
Ilya <53121795+Razent1@users.noreply.github.com>
2022-02-02 18:50:59 +0300

    project

---
+++ Main.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _process(delta):
+	var vector = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	).normalized()
+	$Sprite.position += vector
+	
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
